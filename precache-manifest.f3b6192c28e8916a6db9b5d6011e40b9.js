e09cb5eae5251cd20cf95f4d6cf7d03c2dfbf7ef
rodriguezcravero <nacho.rodriguez85@gmail.com>
2020-08-29 19:18:43 -0300

    Updates

---
+++ precache-manifest.f3b6192c28e8916a6db9b5d6011e40b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48b26cb0d157cee4a9ebb27514fa4038",
+    "url": "/misgastos/index.html"
+  },
+  {
+    "revision": "c658f1fe3baa6adb1f79",
+    "url": "/misgastos/static/css/main.caf1db2f.chunk.css"
+  },
+  {
+    "revision": "d274357ae37698f69cd8",
+    "url": "/misgastos/static/js/2.87902a96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/misgastos/static/js/2.87902a96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c658f1fe3baa6adb1f79",
+    "url": "/misgastos/static/js/main.26f8d0f5.chunk.js"
+  },
+  {
+    "revision": "6489247d829865d8db72",
+    "url": "/misgastos/static/js/runtime-main.c2c68bfa.js"
+  }
+]);
